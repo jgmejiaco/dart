afaca807eae8ff2b3de1ed59e99337e9f2e6f8f4
Juangui <jgmejiaco@gmail.com>
2025-08-08 14:16:55 -0500

    Comentarios

---
+++ ejercicios/01_basico/04_comentarios.dart
@@ -0,0 +1,22 @@
+main() {
+  // Comentario normal
+  //
+
+  ///**
+  ///
+  ///
+  ///
+  ///
+  /// */ 
+  
+  /// Funciona para saludar
+  /// Recibe un [nombre] y se concatena con el [mensaje]
+  saludar(String nombre, String mensaje) {}
+
+  /**
+  * Este archivo sirve para explicar los comentarios
+  *
+  * [creadoEn]: HOy
+  *
+  */ 
+}
